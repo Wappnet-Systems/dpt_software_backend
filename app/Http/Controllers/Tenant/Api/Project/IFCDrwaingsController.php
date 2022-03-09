28f673d54ddc09ff977168cc2db637edd6189fdf
chintanwappnet <chintan.wappnet@gmail.com>
2022-03-09 19:08:57 +0530

    IFC Drawing management APIs

--- app/Http/Controllers/Tenant/Api/Project/IFCDrwaingsController.php
+++ app/Http/Controllers/Tenant/Api/Project/IFCDrwaingsController.php
@@ -10,6 +10,7 @@ use Hyn\Tenancy\Models\Website;
 use App\Models\System\Organization;
 use App\Models\System\User;
 use App\Models\Tenant\ProjectIFCDrwaing;
+use App\Models\Tenant\ProjectActivity;
 use App\Helpers\AppHelper;
 use App\Helpers\UploadFile;
 
@@ -49,12 +50,59 @@ class IFCDrwaingsController extends Controller
 
     public function getIFCDrwaings(Request $request)
     {
-        # code...
+        $limit = !empty($request->limit) ? $request->limit : config('constants.default_per_page_limit');
+        $orderBy = !empty($request->orderby) ? $request->orderby : config('constants.default_orderby');
+
+        $query = ProjectIFCDrwaing::with('project')
+            ->whereProjectId($request->project_id ?? '')
+            ->whereStatus(ProjectIFCDrwaing::STATUS['Active'])
+            ->orderby('id', $orderBy);
+
+        if (isset($request->search) && !empty($request->search)) {
+            $search = trim(strtolower($request->search));
+
+            $query = $query->whereRaw('LOWER(CONCAT(`name`)) LIKE ?', ['%' . $search . '%']);
+
+            $query = $query->orWhereHas('project', function ($query) use ($search) {
+                $query->whereRaw('LOWER(CONCAT(`name`)) LIKE ?', ['%' . $search . '%']);
+            });
+        }
+
+        if ($request->exists('cursor')) {
+            $projectIFCDrawing = $query->cursorPaginate($limit)->toArray();
+        } else {
+            $projectIFCDrawing['data'] = $query->get()->toArray();
+        }
+
+        $results = [];
+        if (!empty($projectIFCDrawing['data'])) {
+            $results = $projectIFCDrawing['data'];
+        }
+
+        if ($request->exists('cursor')) {
+            return $this->sendResponse([
+                'lists' => $results,
+                'per_page' => $projectIFCDrawing['per_page'],
+                'next_page_url' => $projectIFCDrawing['next_page_url'],
+                'prev_page_url' => $projectIFCDrawing['prev_page_url']
+            ], 'Project ifc Drawing List');
+        } else {
+            return $this->sendResponse($results, 'Project ifc Drawing List');
+        }
     }
 
     public function getIFCDrwaingDetails(Request $request)
     {
-        # code...
+        $projectIFCDrawing = ProjectIFCDrwaing::with('project')
+            ->select('id', 'project_id', 'name', 'path', 'location', 'area', 'type', 'status')
+            ->whereId($request->id)
+            ->first();
+
+        if (!isset($projectIFCDrawing) || empty($projectIFCDrawing)) {
+            return $this->sendError('Project ifc drawing does not exist.');
+        }
+
+        return $this->sendResponse($projectIFCDrawing, 'Project ifc drawing details.');
     }
 
     public function addIFCDrwaing(Request $request)
@@ -65,10 +113,12 @@ class IFCDrwaingsController extends Controller
             if (isset($user) && !empty($user)) {
                 $validator = Validator::make($request->all(), [
                     'project_id' => 'required|exists:projects,id',
-                    'name' => 'required|',
-                    'path' => 'required|',
+                    'name' => 'required',
+                    'path' => sprintf('mimes:%s|max:%s', 'pdf,jpeg,jpg,bmp,png', config('constants.organizations.projects.ifc_drawings.upload_image_max_size')),
                     'location' => 'required',
                     'area' => 'required',
+                ], [
+                    'path.max' => 'The file must not be greater than 10mb.',
                 ]);
 
                 if ($validator->fails()) {
@@ -76,6 +126,33 @@ class IFCDrwaingsController extends Controller
                         return $this->sendError('Validation Error.', [$key => $value[0]]);
                     }
                 }
+
+                $IFCDrawing = new ProjectIFCDrwaing();
+                $IFCDrawing->project_id = $request->project_id;
+                $IFCDrawing->name = $request->name;
+                $IFCDrawing->location = $request->location;
+                $IFCDrawing->area = $request->area;
+                $IFCDrawing->created_by = $user->id;
+                $IFCDrawing->created_ip = $request->ip();
+                $IFCDrawing->updated_ip = $request->ip();
+
+                if ($request->hasFile('path')) {
+                    $dirPath = str_replace([':uid:', ':project_uuid:'], [$user->organization_id, $request->project_id], config('constants.organizations.projects.ifc_drawings.file_path'));
+
+                    $IFCDrawing->path = $this->uploadFile->uploadFileInS3($request, $dirPath, 'path');
+
+                    if ($request->file('path')->getClientOriginalExtension() == 'pdf') {
+                        $IFCDrawing->type = ProjectIFCDrwaing::TYPE['PDF'];
+                    } else {
+                        $IFCDrawing->type = ProjectIFCDrwaing::TYPE['Image'];
+                    }
+                }
+
+                if (!$IFCDrawing->save()) {
+                    return $this->sendError('Something went wrong while creating the project ifc drawing');
+                }
+
+                return $this->sendResponse($IFCDrawing, 'Project ifc drawing created successfully.');
             } else {
                 return $this->sendError('User not exists.');
             }
@@ -86,16 +163,140 @@ class IFCDrwaingsController extends Controller
 
     public function updateIFCDrwaing(Request $request)
     {
-        # code...
+        try {
+            $user = $request->user();
+
+            if (isset($user) && !empty($user)) {
+                $validator = Validator::make($request->all(), [
+                    'id' => 'required',
+                    'name' => 'required',
+                    'path' => sprintf('mimes:%s|max:%s', 'pdf,jpeg,jpg,bmp,png', config('constants.organizations.projects.ifc_drawings.upload_image_max_size')),
+                    'location' => 'required',
+                    'area' => 'required',
+                ], [
+                    'path.max' => 'The file must not be greater than 10mb.',
+                ]);
+
+                if ($validator->fails()) {
+                    foreach ($validator->errors()->messages() as $key => $value) {
+                        return $this->sendError('Validation Error.', [$key => $value[0]]);
+                    }
+                }
+
+                $projectIFCDrawing = ProjectIFCDrwaing::whereId($request->id)
+                    ->first();
+
+                if (!isset($projectIFCDrawing) || empty($projectIFCDrawing)) {
+                    return $this->sendError('Project ifc drawing does not exist.');
+                }
+
+                if ($request->filled('name')) $projectIFCDrawing->name = $request->name;
+                if ($request->filled('location')) $projectIFCDrawing->location = $request->location;
+                if ($request->filled('area')) $projectIFCDrawing->area = $request->area;
+
+                if ($request->hasFile('path')) {
+                    if (isset($projectIFCDrawing->path) && !empty($projectIFCDrawing->path)) {
+                        $this->uploadFile->deleteFileFromS3($projectIFCDrawing->path);
+                    }
+
+                    $dirPath = str_replace([':uid:', ':project_uuid:'], [$user->organization_id, $request->project_id], config('constants.organizations.projects.ifc_drawings.file_path'));
+
+                    $projectIFCDrawing->path = $this->uploadFile->uploadFileInS3($request, $dirPath, 'path');
+
+                    if ($request->file('path')->getClientOriginalExtension() == 'pdf') {
+                        $projectIFCDrawing->type = ProjectIFCDrwaing::TYPE['PDF'];
+                    } else {
+                        $projectIFCDrawing->type = ProjectIFCDrwaing::TYPE['Image'];
+                    }
+                }
+                $projectIFCDrawing->updated_ip = $request->ip();
+
+                if (!$projectIFCDrawing->save()) {
+                    return $this->sendError('Something went wrong while updating the project ifc drawing');
+                }
+
+                return $this->sendResponse($projectIFCDrawing, 'Project ifc drawing updated successfully.');
+            } else {
+                return $this->sendError('User not exists.');
+            }
+        } catch (\Exception $e) {
+            return $this->sendError($e->getMessage());
+        }
     }
 
     public function deleteIFCDrwaing(Request $request)
     {
-        # code...
+        try {
+            $validator = Validator::make($request->all(), [
+                'id' => 'required',
+            ]);
+
+            if ($validator->fails()) {
+                foreach ($validator->errors()->messages() as $key => $value) {
+                    return $this->sendError('Validation Error.', [$key => $value[0]]);
+                }
+            }
+
+            $projectIFCDrawing = ProjectIFCDrwaing::whereId($request->id)
+                ->first();
+
+            if (!isset($projectIFCDrawing) || empty($projectIFCDrawing)) {
+                return $this->sendError('Project ifc drawing does not exist.');
+            }
+
+            if (ProjectActivity::whereIn('status', [ProjectActivity::STATUS['Start'], ProjectActivity::STATUS['Hold'], ProjectActivity::STATUS['Pending']])
+                ->whereProjectDrowingId($request->id)
+                ->exists()) {
+                return $this->sendError('Project ifc drawing assign to project activities.');
+            }
+
+            if (isset($projectIFCDrawing->path) && !empty($projectIFCDrawing->path)) {
+                $this->uploadFile->deleteFileFromS3($projectIFCDrawing->path);
+            }
+
+            $projectIFCDrawing->delete();
+
+            return $this->sendResponse([], 'Project ifc drawing deleted successfully.');
+        } catch (\Exception $e) {
+            return $this->sendError($e->getMessage());
+        }
     }
 
     public function changeIFCDrwaingStatus(Request $request)
     {
-        # code...
+        try {
+            $validator = Validator::make($request->all(), [
+                'id' => 'required',
+            ]);
+
+            if ($validator->fails()) {
+                foreach ($validator->errors()->messages() as $key => $value) {
+                    return $this->sendError('Validation Error.', [$key => $value[0]]);
+                }
+            }
+
+            $projectIFCDrawing = ProjectIFCDrwaing::whereId($request->id)
+                ->first();
+
+            if (!isset($projectIFCDrawing) || empty($projectIFCDrawing)) {
+                return $this->sendError('Project ifc drawing does not exist.');
+            }
+
+            $projectIFCDrawing->deleted_at = null;
+            $projectIFCDrawing->status = $request->status;
+            $projectIFCDrawing->save();
+
+            if ($projectIFCDrawing->status == ProjectIFCDrwaing::STATUS['Deleted']) {
+                if (isset($projectIFCDrawing->path) && !empty($projectIFCDrawing->path)) {
+                    $this->uploadFile->deleteFileFromS3($projectIFCDrawing->path);
+                }
+
+                $projectIFCDrawing->delete();
+            }
+
+            return $this->sendResponse($projectIFCDrawing, 'Status changed successfully.');
+        } catch (\Exception $e) {
+            return $this->sendError($e->getMessage());
+        }
     }
 }
--- app/Models/Tenant/ProjectIFCDrwaing.php
+++ app/Models/Tenant/ProjectIFCDrwaing.php
@@ -5,6 +5,7 @@ namespace App\Models\Tenant;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Helpers\UploadFile;
 
 class ProjectIFCDrwaing extends Model
 {
@@ -15,7 +16,7 @@ class ProjectIFCDrwaing extends Model
 
     protected $guarded = [];
 
-    protected $appends = ['status_name', 'type_name'];
+    protected $appends = ['status_name', 'type_name', 'file_path'];
 
     const STATUS = [
         'Active' => 1,
@@ -58,5 +59,22 @@ class ProjectIFCDrwaing extends Model
         }
 
         return null;
+    }
+
+    public function getFilePathAttribute()
+    {
+        if ($this->path) {
+            $uploadFile = new UploadFile();
+
+            return $uploadFile->getS3FilePath('path', $this->path);
+        }
+
+        return null;
+    }
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class, 'project_id', 'id')
+            ->select('id', 'name', 'logo', 'address', 'lat', 'long', 'city', 'state', 'country', 'zip_code', 'start_date', 'end_date', 'cost', 'status');
     }
 }
