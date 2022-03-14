459347aa98d0b0f8b842dd67a11ff19a8ac763e6
akhilwappnet <akhil.wappnet@gmail.com>
2022-03-14 07:41:03 -0600

    Fixed seeder issues and Allocate Machinery APIs

--- database/seeders/Tenant/ActivitySubActivityCategorySeeder.php
+++ database/seeders/Tenant/ActivitySubActivityCategorySeeder.php
@@ -17,54 +17,42 @@ class ActivitySubActivityCategorySeeder extends Seeder
     public function run()
     {
         $activityCategoryArr = [
-
             'Formwork' => [
                 'm2' => ['Stab Docking', 'Foundating shuttering', 'Column shuttering', 'Shear wall', 'Retaining wall', 'Upstand', 'Core wall', 'Grade slab', 'DownStand']
             ],
-
             'Staging' => [
                 'm3' => ['Docking']
             ],
-
             'Steel reinforcement' => [
                 'kg' =>  ['Slab', 'Foundation', 'Foundation-mat', 'Column', 'Shear wall', 'Retaining wall', 'Corewall', 'Grade slab', 'Upstand', 'Down Stand']
             ],
-
             'Concreting' => [
                 'm3' => ['Foundation', 'Foundation-mat', 'Column', 'Shear wall', 'Retaining wall', 'Blinding', 'Screed', 'Corewall']
             ],
-
             'Surface Preparation' => [
                 'm2' => ['Water Proofing', 'Concrete repair', 'Vertical element', 'Soil formation', 'Post-tension']
             ],
-
             'Post-tension' => [
                 'lmorno' => ['Cable laying', 'Anchor-block and grout pipes']
             ],
-
             'Block work' => [
                 'm2' => ['100 mm hollow', '150 mm hollowinsolated', '200 mm hollowinsolated', '100 mm solid', '1500 mm solid', '200 mm solid']
             ],
-
             'Painting' => [
                 'm2' => ['Primer', 'Intermediate coat', 'Sanding', 'Final coat']
             ],
-
             'Tile fixing' => [
                 'm2' => ['Floor filing', 'Wall tiling']
             ],
         ];
 
         foreach ($activityCategoryArr as $categoryKey => $unitTypeNameArr) {
-
             $activityCategory = new ActivityCategory();
             $activityCategory->name = $categoryKey;
             $activityCategory->save();
 
             foreach ($unitTypeNameArr as $unitTypeNameKey => $subCategoryValueArr) {
-
                 foreach ($subCategoryValueArr as $subCategoryValue) {
-
                     $subCategory = new ActivitySubCategory();
 
                     $unitTypeCheck = UnitType::whereName($unitTypeNameKey)->first();
@@ -73,15 +61,12 @@ class ActivitySubActivityCategorySeeder extends Seeder
                         $unitTypes = new UnitType();
                         $unitTypes->name = $unitTypeNameKey;
                         $unitTypes->save();
-
                     }
 
                     $subCategory->activity_category_id = $activityCategory->id;
                     $subCategory->name = $subCategoryValue;
                     $subCategory->unit_type_id = !empty($unitTypeCheck) ? $unitTypeCheck->id : $unitTypes->id;
-
-                    $subCategory->save();   
-
+                    $subCategory->save();
                 }
             }
         }
