ea3478f3da761d9585c77ff5fd5893a0ea834de4
chintanwappnet <chintan.wappnet@gmail.com>
2022-04-01 14:40:40 +0530

    Inspection Management API (working)

---
+++ app/Http/Controllers/Tenant/Api/Project/InspectionController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Tenant\Api\Project;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Hyn\Tenancy\Models\Hostname;
+use Hyn\Tenancy\Models\Website;
+use App\Models\System\Organization;
+use App\Models\System\User;
+use App\Models\Tenant\ProjectInspection;
+use App\Helpers\AppHelper;
+
+class InspectionController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware(function ($request, $next) {
+            $user = $request->user();
+
+            if (isset($user) && !empty($user)) {
+                if ($user->role_id == User::USER_ROLE['SUPER_ADMIN']) {
+                    return $this->sendError('You have no rights to access this module.');
+                }
+
+                $hostnameId = Organization::whereId($user->organization_id)->value('hostname_id');
+
+                $hostname = Hostname::whereId($hostnameId)->first();
+                $website = Website::whereId($hostname->website_id)->first();
+
+                $environment = app(\Hyn\Tenancy\Environment::class);
+                $hostname = Hostname::whereWebsiteId($website->id)->first();
+
+                $environment->tenant($website);
+                $environment->hostname($hostname);
+
+                AppHelper::setDefaultDBConnection();
+            }
+
+            return $next($request);
+        });
+    }
+    
+}
---
+++ database/migrations/tenant/2022_04_01_071312_create_projects_inspections_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsInspectionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects_inspections', function (Blueprint $table) {
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_general_ci';
+
+            $table->id();
+            $table->foreignId('project_activity_id')->nullable()->constrained('projects_activities')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('project_allocate_material_id')->nullable()->constrained('projects_activities_allocate_materials')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->string('inspection_no', 30)->nullable();
+            $table->date('inspection_date');
+            $table->date('approve_reject_date')->nullable();
+            $table->string('location', 50)->nullable();
+            $table->string('document', 200)->nullable();
+            $table->tinyInteger('inspection_type')->comment('1 - Internal, 2 - External');
+            $table->tinyInteger('type')->comment('1 - Activity, 2 - Material');
+            $table->tinyInteger('inspection_status')->default(1)->comment('1 - Pending, 2 - Approved, 3 - Rejected');
+            $table->longText('comments')->nullable();
+            $table->longText('reason')->nullable();
+            $table->tinyInteger('status')->default(1)->comment('1 - Active, 2 - In Active, 3 - Deleted');
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('updated_by');
+            $table->ipAddress('created_ip')->nullable();
+            $table->ipAddress('updated_ip')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects_inspections');
+    }
+}
