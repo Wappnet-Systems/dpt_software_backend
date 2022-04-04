9a3a52143d6c78253f674dc8ea2ec75f051a858d
chintanwappnet <chintan.wappnet@gmail.com>
2022-04-04 19:39:56 +0530

    Inspection Management API(done)

--- app/Http/Controllers/Tenant/Api/Project/InspectionController.php
+++ app/Http/Controllers/Tenant/Api/Project/InspectionController.php
@@ -11,11 +11,16 @@ use App\Models\System\Organization;
 use App\Models\System\User;
 use App\Models\Tenant\ProjectInspection;
 use App\Helpers\AppHelper;
+use App\Helpers\UploadFile;
 
 class InspectionController extends Controller
 {
+    protected $uploadFile;
+
     public function __construct()
     {
+        $this->uploadFile = new UploadFile();
+
         $this->middleware(function ($request, $next) {
             $user = $request->user();
 
@@ -41,5 +46,297 @@ class InspectionController extends Controller
             return $next($request);
         });
     }
-    
+
+    public function getProjectInspection(Request $request)
+    {
+        $limit = !empty($request->limit) ? $request->limit : config('constants.default_per_page_limit');
+        $orderBy = !empty($request->orderby) ? $request->orderby : config('constants.default_orderby');
+
+        try {
+            $query = ProjectInspection::with('projectActivity')
+                ->orderBy('id', $orderBy);
+
+            $totalQuery = $query;
+            $totalQuery = $totalQuery->count();
+
+            if ($request->exists('cursor')) {
+                $projectInspection = $query->cursorPaginate($limit)->toArray();
+            } else {
+                $projectInspection['data'] = $query->get()->toArray();
+            }
+
+            $results = [];
+            if (!empty($projectInspection['data'])) {
+                $results = $projectInspection['data'];
+            }
+
+            if ($request->exists('cursor')) {
+                return $this->sendResponse([
+                    'lists' => $results,
+                    'total' => $totalQuery,
+                    'per_page' => $projectInspection['per_page'],
+                    'next_page_url' => ltrim(str_replace($projectInspection['path'], "", $projectInspection['next_page_url']), "?cursor="),
+                    'prev_page_url' => ltrim(str_replace($projectInspection['path'], "", $projectInspection['prev_page_url']), "?cursor=")
+                ], 'Project inspection List.');
+            } else {
+                return $this->sendResponse($results, 'Project inspection List.');
+            }
+        } catch (\Exception $e) {
+            return $this->sendError($e->getMessage());
+        }
+    }
+
+    public function getProjectInspectionDetails(Request $request, $id = null)
+    {
+        $projectInspection = ProjectInspection::with('projectActivity')
+            ->whereId($request->id)
+            ->first();
+
+        if (!isset($projectInspection) || empty($projectInspection)) {
+            return $this->sendError('Project inspection does not exists.');
+        }
+
+        return $this->sendResponse($projectInspection, 'Project inspection details.');
+    }
+
+    public function addProjectInspection(Request $request)
+    {
+        try {
+            $user = $request->user();
+
+            if (isset($user) && !empty($user)) {
+                $validator = Validator::make($request->all(), [
+                    'project_activity_id' => 'required|exists:projects_activities,id',
+                    'inspection_no' => 'required|numeric|digits_between:10,30',
+                    'inspection_date' => 'required|date_format:Y-m-d',
+                    'location' => 'required',
+                    'document' => 'mimes:pdf|max:10240',
+                    'inspection_type' => 'required',
+                ], [
+                    'document.max' => 'The file must not be greater than 10mb.',
+                ]);
+
+                if ($validator->fails()) {
+                    foreach ($validator->errors()->messages() as $key => $value) {
+                        return $this->sendError('Validation Error.', [$key => $value[0]]);
+                    }
+                }
+
+                $projectInspection = new ProjectInspection();
+                $projectInspection->project_activity_id = $request->project_activity_id;
+                $projectInspection->inspection_no = $request->inspection_no;
+                $projectInspection->inspection_date = date('Y-m-d', strtotime($request->inspection_date));
+                $projectInspection->location = $request->location;
+                $projectInspection->inspection_type = $request->inspection_type;
+                $projectInspection->type = !empty($request->type) ? $request->type : ProjectInspection::TYPE['Activity'];
+
+                if ($request->hasFile('document')) {
+                    $dirPath = str_replace([':uid:'], [$user->organization_id], config('constants.organizations.projects.inspection.document_path'));
+
+                    $projectInspection->document = $this->uploadFile->uploadFileInS3($request, $dirPath, 'document');
+                }
+
+                $projectInspection->created_by = $user->id;
+                $projectInspection->updated_by = $user->id;
+                $projectInspection->created_ip = $request->ip();
+                $projectInspection->updated_ip = $request->ip();
+
+                if (!$projectInspection->save()) {
+                    return $this->sendError('Something went wrong while creating the project inspection.');
+                }
+
+                return $this->sendResponse([$projectInspection], 'Project inspection created successfully.');
+            } else {
+                return $this->sendError('User not exists.');
+            }
+        } catch (\Exception $e) {
+            return $this->sendError($e->getMessage());
+        }
+    }
+
+    public function updateProjectInspection(Request $request, $id = null)
+    {
+        try {
+            $user = $request->user();
+
+            if (isset($user) && !empty($user)) {
+                $validator = Validator::make($request->all(), [
+                    'inspection_no' => 'required|numeric|digits_between:10,30',
+                    'inspection_date' => 'required|date_format:Y-m-d',
+                    'location' => 'required',
+                    'document' => 'mimes:pdf|max:10240',
+                    'inspection_type' => 'required',
+                ], [
+                    'document.max' => 'The file must not be greater than 10mb.'
+                ]);
+
+                if ($validator->fails()) {
+                    foreach ($validator->errors()->messages() as $key => $value) {
+                        return $this->sendError('Validation Error.', [$key => $value[0]]);
+                    }
+                }
+
+                $projectInspection = ProjectInspection::whereId($request->id)
+                    ->where('inspection_status', ProjectInspection::INC_STATUS['Pending'])
+                    ->first();
+
+                if (!isset($projectInspection) || empty($projectInspection)) {
+                    return $this->sendError('Project inspection does not exists.');
+                }
+
+                if ($request->filled('inspection_no')) $projectInspection->inspection_no = $request->inspection_no;
+                if ($request->filled('inspection_date')) $projectInspection->inspection_date = date('Y-m-d', strtotime($request->inspection_date));
+                if ($request->filled('location')) $projectInspection->location = $request->location;
+                if ($request->filled('inspection_type')) $projectInspection->inspection_type = $request->inspection_type;
+                if ($request->filled('type')) $projectInspection->type = !empty($request->type) ? $request->type : ProjectInspection::TYPE['Activity'];
+
+                if ($request->hasFile('document')) {
+                    if (isset($projectInspection->document) && !empty($projectInspection->document)) {
+                        $this->uploadFile->deleteFileFromS3($projectInspection->document);
+                    }
+
+                    $dirPath = str_replace([':uid:'], [$user->organization_id], config('constants.organizations.projects.inspection.document_path'));
+
+                    $projectInspection->document = $this->uploadFile->uploadFileInS3($request, $dirPath, 'document');
+                }
+
+                $projectInspection->updated_by = $user->id;
+                $projectInspection->updated_ip = $request->ip();
+
+                if (!$projectInspection->save()) {
+                    return $this->sendError('Something went wrong while updating the project inspection');
+                }
+
+                return $this->sendResponse($projectInspection, 'Project inspection updated successfully.');
+            } else {
+                return $this->sendError('User not exists.');
+            }
+        } catch (\Exception $e) {
+            return $this->sendError($e->getMessage());
+        }
+    }
+
+    public function deleteProjectInspection(Request $request, $id = null)
+    {
+        try {
+            $user = $request->user();
+
+            if (isset($user) && !empty($user)) {
+                $projectInspection = ProjectInspection::whereId($request->id)
+                    ->where('inspection_status', ProjectInspection::INC_STATUS['Pending'])
+                    ->first();
+
+                if (!isset($projectInspection) || empty($projectInspection)) {
+                    return $this->sendError('Project inspection does not exists.');
+                }
+
+                $projectInspection->delete();
+
+                return $this->sendResponse([], 'Project inspection deleted Successfully.');
+            } else {
+                return $this->sendError('User does not exists.');
+            }
+        } catch (\Exception $e) {
+            return $this->sendError($e->getMessage());
+        }
+    }
+
+    public function projectInspectionChangeStatus(Request $request, $id = null)
+    {
+        try {
+            $user = $request->user();
+
+            if (isset($user) && !empty($user)) {
+                $validator = Validator::make($request->all(), [
+                    'status' => 'required'
+                ]);
+
+                if ($validator->fails()) {
+                    foreach ($validator->errors()->messages() as $key => $value) {
+                        return $this->sendError('Validation Error.', [$key => $value[0]]);
+                    }
+                }
+
+                if (!in_array($request->status, ProjectInspection::STATUS)) {
+                    return $this->sendError('Invalid status requested.');
+                }
+
+                $projectInspection = ProjectInspection::whereId($request->id)
+                    ->where('inspection_status', ProjectInspection::INC_STATUS['Pending'])
+                    ->first();
+
+                if (!isset($projectInspection) || empty($projectInspection)) {
+                    return $this->sendError('Project inspection does not exists.');
+                }
+
+                $projectInspection->status = $request->status;
+                $projectInspection->updated_by = $user->id;
+                $projectInspection->updated_ip = $request->ip();
+                $projectInspection->save();
+
+                return $this->sendResponse($projectInspection, 'Status changed successfully.');
+            } else {
+                return $this->sendError('User not exists.');
+            }
+        } catch (\Exception $e) {
+            return $this->sendError($e->getMessage());
+        }
+    }
+
+    public function projectInspectionApproveReject(Request $request, $id = null)
+    {
+        try {
+            $user = $request->user();
+
+            if (isset($user) && !empty($user)) {
+                $validator = Validator::make($request->all(), [
+                    'inspection_status' => 'required'
+                ]);
+
+                if ($validator->fails()) {
+                    foreach ($validator->errors()->messages() as $key => $value) {
+                        return $this->sendError('Validation Error.', [$key => $value[0]]);
+                    }
+                }
+
+                $projectInspection = ProjectInspection::whereId($request->id)
+                    ->where('inspection_status', ProjectInspection::INC_STATUS['Pending'])
+                    ->first();
+
+                if (!isset($projectInspection) || empty($projectInspection)) {
+                    return $this->sendError('Project inspection does not exists.');
+                }
+
+                if ($request->inspection_status == ProjectInspection::INC_STATUS['Rejected']) {
+                    $validator = Validator::make($request->all(), [
+                        'reason' => 'required'
+                    ]);
+
+                    if ($validator->fails()) {
+                        foreach ($validator->errors()->messages() as $key => $value) {
+                            return $this->sendError('Validation Error.', [$key => $value[0]]);
+                        }
+                    }
+                }
+
+                if ($request->inspection_status == ProjectInspection::INC_STATUS['Approved']) {
+                    $projectInspection->inspection_status = ProjectInspection::INC_STATUS['Approved'];
+                } elseif ($request->inspection_status == ProjectInspection::INC_STATUS['Rejected']) {
+                    $projectInspection->inspection_status = ProjectInspection::INC_STATUS['Rejected'];
+                }
+
+                $projectInspection->approve_reject_date = date('Y-m-d');
+                $projectInspection->reason = $request->reason;
+                $projectInspection->updated_by = $user->id;
+                $projectInspection->updated_ip = $request->ip();
+                $projectInspection->save();
+
+                return $this->sendResponse($projectInspection, 'Status changed successfully.');
+            } else {
+                return $this->sendError('User does not exists.');
+            }
+        } catch (\Exception $e) {
+            return $this->sendError($e->getMessage());
+        }
+    }
 }
--- app/Models/Tenant/ProjectInspection.php
+++ app/Models/Tenant/ProjectInspection.php
@@ -4,6 +4,7 @@ namespace App\Models\Tenant;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Helpers\UploadFile;
 
 class ProjectInspection extends Model
 {
@@ -12,4 +13,109 @@ class ProjectInspection extends Model
     protected $table = "projects_inspections";
 
     protected $guarded = [];
+
+    protected $appends = ['inspection_type_name', 'type_name', 'inspection_status_name', 'status_name', 'document_path'];
+
+    const INC_TYPE = [
+        'Internal' => 1,
+        'External' => 2
+    ];
+
+    const TYPE = [
+        'Activity' => 1,
+        'Material' => 2
+    ];
+
+    const INC_STATUS = [
+        'Pending' => 1,
+        'Approved' => 2,
+        'Rejected' => 3
+    ];
+
+    const STATUS = [
+        'Active' => 1,
+        'In Active' => 2,
+        'Deleted' => 3,
+    ];
+
+    /**
+     * Get the inspection type name.
+     *
+     * @return string
+     */
+    public function getinspectionTypeNameAttribute()
+    {
+        $flipStatus = array_flip(self::INC_TYPE);
+
+        if (isset($flipStatus[$this->inspection_type]) && !empty($flipStatus[$this->inspection_type])) {
+            return "{$flipStatus[$this->inspection_type]}";
+        }
+
+        return null;
+    }
+
+    /**
+     * Get the type name.
+     *
+     * @return string
+     */
+    public function getTypeNameAttribute()
+    {
+        $flipStatus = array_flip(self::TYPE);
+
+        if (isset($flipStatus[$this->type]) && !empty($flipStatus[$this->type])) {
+            return "{$flipStatus[$this->type]}";
+        }
+
+        return null;
+    }
+
+    /**
+     * Get the inspection status name.
+     *
+     * @return string
+     */
+    public function getInspectionStatusNameAttribute()
+    {
+        $flipStatus = array_flip(self::INC_STATUS);
+
+        if (isset($flipStatus[$this->inspection_status]) && !empty($flipStatus[$this->inspection_status])) {
+            return "{$flipStatus[$this->inspection_status]}";
+        }
+
+        return null;
+    }
+
+    /**
+     * Get the status name.
+     *
+     * @return string
+     */
+    public function getStatusNameAttribute()
+    {
+        $flipStatus = array_flip(self::STATUS);
+
+        if (isset($flipStatus[$this->status]) && !empty($flipStatus[$this->status])) {
+            return "{$flipStatus[$this->status]}";
+        }
+
+        return null;
+    }
+
+    public function getDocumentPathAttribute()
+    {
+        if ($this->document) {
+            $uploadFile = new UploadFile();
+
+            return $uploadFile->getS3FilePath('document', $this->document);
+        }
+
+        return null;
+    }
+
+    public function projectActivity()
+    {
+        return $this->belongsTo(ProjectActivity::class, 'project_activity_id', 'id')
+            ->select('id', 'project_id', 'activity_sub_category_id', 'project_drowing_id', 'name', 'scaffold_number', 'start_date', 'end_date', 'actual_start_date', 'actual_end_date', 'location', 'level', 'actual_area', 'completed_area', 'cost', 'status', 'productivity_rate');
+    }
 }
