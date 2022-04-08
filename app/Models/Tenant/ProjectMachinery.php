3520805a136934638f3760bc32d63266743bc8b1
chintanwappnet <chintan.wappnet@gmail.com>
2022-04-08 12:52:00 +0530

    change migration of machineries to projects_machineries and change all related Apis and model, relationships

--- app/Models/Tenant/ProjectMachinery.php
+++ app/Models/Tenant/ProjectMachinery.php
@@ -6,12 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Machinery extends Model
+class ProjectMachinery extends Model
 {
     use HasFactory;
     use SoftDeletes;
 
-    protected $table = "machineries";
+    protected $table = "projects_machineries";
 
     protected $guarded = [];
 
