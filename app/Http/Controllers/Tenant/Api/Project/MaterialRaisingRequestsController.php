f90db538ce00a3f548c97d2627ddfde701415c1e
chintanwappnet <chintan.wappnet@gmail.com>
2022-04-15 13:28:30 +0530

    activity document image issue solved and material raising request add api

--- app/Http/Controllers/Tenant/Api/Project/MaterialRaisingRequestsController.php
+++ app/Http/Controllers/Tenant/Api/Project/MaterialRaisingRequestsController.php
@@ -53,6 +53,42 @@ class MaterialRaisingRequestsController extends Controller
 
     public function addMaterialRaisingRequest(Request $request)
     {
+        try {
+            $user = $request->user();
+
+            if (isset($user) && !empty($user)) {
+                $validator = Validator::make($request->all(), [
+                    'project_id' => 'required|exists:projects,id',
+                    'material_type_id' => 'required|exists:material_types,id',
+                    'unit_type_id' => 'required|exists:unit_types,id',
+                    'quantity' => 'required|gt:0'
+                ]);
+
+                if ($validator->fails()) {
+                    foreach ($validator->errors()->messages() as $key => $value) {
+                        return $this->sendError('Validation Error.', [$key => $value[0]]);
+                    }
+                }
+
+                $materialRaisingRequest = new ProjectMaterialRaisingRequest();
+                $materialRaisingRequest->project_id = $request->project_id;
+                $materialRaisingRequest->material_type_id = $request->material_type_id;
+                $materialRaisingRequest->unit_type_id = $request->unit_type_id;
+                $materialRaisingRequest->quantity = $request->quantity;
+                $materialRaisingRequest->created_ip = $request->ip();
+                $materialRaisingRequest->updated_ip = $request->ip();
+
+                if (!$materialRaisingRequest->save()) {
+                    return $this->sendError('Something went wrong while creating the project material raising request.');
+                }
+
+                return $this->sendResponse($materialRaisingRequest, 'Project material raising request created successfully');
+            } else {
+                return $this->sendError('User does not exist');
+            }
+        } catch (\Exception $e) {
+            return $this->sendError($e->getMessage());
+        }
     }
 
     public function updateMaterialRaisingRequest(Request $request)
