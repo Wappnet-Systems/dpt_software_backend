2f9d7234a09ab6ff3c0defd6ec16c22b15fe6724
akhilwappnet <akhil.wappnet@gmail.com>
2022-05-04 23:44:04 -0600

    Activity changes

---
+++ database/migrations/tenant/2022_03_04_115443_create_projects_main_activities_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsMainActivitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects_main_activities', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('project_id')->constrained('projects')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('activity_sub_category_id')->constrained('activity_sub_categories')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->string('name', 50);
+            $table->tinyInteger('status')->default(1)->comment('1 - Active, 2 - In Active, 3 - Deleted');
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->ipAddress('created_ip')->nullable();
+            $table->ipAddress('updated_ip')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects_main_activities');
+    }
+}
