32703d1725846cacb62f652c76d4ef0d049cd9dc
chintanwappnet <chintan.wappnet@gmail.com>
2022-05-06 12:57:21 +0530

    Minimum stock alert

--- routes/api.php
+++ routes/api.php
@@ -196,6 +196,7 @@ Route::middleware('auth:sanctum')->group(function () {
 
     /* Projects Inventories Stocks Routes */
     Route::get('project/inventory/stock/get/lists', [InventoryStocksController::class, 'getInventoryStocks']);
+    Route::get('project/inventory/minimum-quantity/lists', [InventoryStocksController::class, 'getMinimumQuantity']);
 
     /* Projects Inventories Minimum Quantity Update Routes */
     Route::put('project/inventory/minimum-quantity/update/{projectInventoryId}', [InventoryStocksController::class, 'updateMinimunQuntity']);
