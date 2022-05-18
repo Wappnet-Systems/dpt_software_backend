a8ac01eb2318447b8c8fa7f3be8fb75d23f4d98e
chintanwappnet <chintan.wappnet@gmail.com>
2022-05-18 16:03:28 +0530

    Set predefined permission for all roles by seeder

--- database/seeders/Tenant/RoleSeeder.php
+++ database/seeders/Tenant/RoleSeeder.php
@@ -36,5 +36,151 @@ class RoleSeeder extends Seeder
             $role->name = $rVal['name'];
             $role->save();
         }
+
+        // predefine permission
+
+        // role Manager 
+
+        $submodulePermission = [
+            [
+                'role_id' => 4,
+                'sub_module_permission' => [
+                    [
+                        'sub_module_id' => 2,
+                        'is_list' => true,
+                    ],
+                    [
+                        'sub_module_id' => 3,
+                        'is_list' => true,
+                        'is_create' => true,
+                        'is_edit' => true,
+                        'is_delete' => true,
+                        'is_view' => true,
+                    ],
+                    [
+                        'sub_module_id' => 14,
+                        'is_list' => true,
+                        'is_create' => true,
+                        'is_edit' => true,
+                        'is_delete' => true,
+                        'is_view' => true,
+                        'is_comment' => true,
+                    ],
+                    [
+                        'sub_module_id' => 9,
+                        'is_list' => true,
+                        'is_create' => true,
+                        'is_edit' => true,
+                        'is_delete' => true,
+                        'is_view' => true,
+                    ],
+                    [
+                        'sub_module_id' => 15,
+                        'is_list' => true,
+                        'is_create' => true,
+                        'is_edit' => true,
+                        'is_delete' => true,
+                        'is_view' => true,
+                    ],
+                    [
+                        'sub_module_id' => 16,
+                        'is_list' => true,
+                        'is_create' => true,
+                        'is_edit' => true,
+                        'is_delete' => true,
+                        'is_view' => true,
+                    ],
+                    [
+                        'sub_module_id' => 17,
+                        'is_list' => true,
+                        'is_create' => true,
+                        'is_edit' => true,
+                        'is_delete' => true,
+                        'is_view' => true,
+                    ],
+                    [
+                        'sub_module_id' => 18,
+                        'is_list' => true,
+                        'is_create' => true,
+                        'is_edit' => true,
+                        'is_delete' => true,
+                        'is_view' => true,
+                    ],
+                    [
+                        'sub_module_id' => 19,
+                        'is_list' => true,
+                        'is_create' => true,
+                        'is_edit' => true,
+                        'is_delete' => true,
+                        'is_view' => true,
+                    ],
+                    [
+                        'sub_module_id' => 20,
+                        'is_list' => true,
+                        'is_create' => true,
+                        'is_edit' => true,
+                        'is_delete' => true,
+                        'is_view' => true,
+                    ],
+                    [
+                        'sub_module_id' => 10,
+                        'is_list' => true,
+                        'is_create' => true,
+                        'is_edit' => true,
+                        'is_delete' => true,
+                        'is_view' => true,
+                    ],
+                    [
+                        'sub_module_id' => 11,
+                        'is_list' => true,
+                        'is_create' => true,
+                        'is_edit' => true,
+                        'is_delete' => true,
+                        'is_view' => true,
+                    ],
+                    [
+                        'sub_module_id' => 12,
+                        'is_list' => true,
+                        'is_create' => true,
+                        'is_edit' => true,
+                        'is_delete' => true,
+                        'is_view' => true,
+                    ],
+                    [
+                        'sub_module_id' => 13,
+                        'is_list' => true,
+                        'is_edit' => true,
+                        'is_view' => true,
+                    ],
+                    [
+                        'sub_module_id' => 21,
+                        'is_list' => true,
+                        'is_create' => true,
+                        'is_edit' => true,
+                        'is_delete' => true,
+                        'is_view' => true,
+                        'is_approve_reject' => true,
+                    ],
+                    [
+                        'sub_module_id' => 22,
+                        'is_list' => true,
+                        'is_create' => true,
+                        'is_edit' => true,
+                        'is_delete' => true,
+                        'is_view' => true,
+                        'is_assign' => true
+                    ],
+                    [
+                        'sub_module_id' => 23,
+                        'is_list' => true,
+                        'is_create' => true,
+                        'is_edit' => true,
+                        'is_delete' => true,
+                        'is_view' => true,
+                        'is_assign' => true
+                    ],
+                ]
+            ]
+        ];
     }
 }
